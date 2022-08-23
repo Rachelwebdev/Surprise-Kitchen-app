f63d586b30d3e36e0a82635911d3af4f9d011810
Rachel Isaac <rachelisaac995@gmail.com>
2022-08-23 21:02:57 +0400

    Fetched data from  API

--- src/detailedAPI-content.js
+++ src/detailedAPI-content.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MealPicture from "./meal-picture";
 import MealVideo from "./meal-video";
 import RecipeInstruction from "./recipe-instructions";
 import Ingredients from "./ingredients";
 
 const DetailedApi = function () {
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/random.php`
+      );
+      let actualdata = await response.json();
+
+      console.log(actualdata);
+    }
+    getData();
+  }, []);
   return (
     <>
       <h1 className="heading">Name of Dish:</h1>
