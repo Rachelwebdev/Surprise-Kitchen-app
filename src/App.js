e950154e61d9662c2503e3dc26fc9d8d86ffaa2e
Rachel Isaac <rachelisaac995@gmail.com>
2022-09-30 10:58:49 +0400

    deleted the detailedAPI-component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Surprise from "./surprise";
-import DetailedApi from "./detailedAPI-content";
+import Surprise from "./Components/surprise";
 
 function App() {
   return (
     <div className="App">
       <Surprise />
-      <DetailedApi />
     </div>
   );
 }
